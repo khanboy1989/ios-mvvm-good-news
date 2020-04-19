51c97880b307bab06e8357ad12abe26539c67a7e
Serhan Khan <serhan@cocooncreations.net>
2020-04-19 11:48:52 +0300

    Initial Commit

---
+++ GoodNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodNews
+//
+//  Created by Serhan Khan on 19/04/2020.
+//  Copyright © 2020 Serhan Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
